3cb32cc4f4a06fa1c1258cb53eab46585c37af62
Dirk Fortmeier <mail@dfortmeier.de>
2023-12-28 19:45:16 +0100

    creating helper.py to clean up

---
+++ helper.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from openai import OpenAI
+
+from emobots.emobot import Emobot
+from emobots.personas import get_all_personas
+
+import api_key
+
+def initialize_friends():
+    personas = get_all_personas()
+
+    friends = {}
+
+    for id, persona in personas.items():
+        client = OpenAI(api_key=api_key.api_key)
+        name = persona["name"]
+        person_desc = persona["description"]
+
+        bot = Emobot(client, name, person_desc)
+        friends[id] = bot
+        print("Created ", name)
+
+    st.session_state.friends = friends
+
+def show_sidebar():
+    with st.sidebar:
+        for id, friend in st.session_state.friends.items():
+            if st.button(friend.name):
+                st.session_state.current_chat_id = id
+
+def show_current_chat_tail():
+    friend = st.session_state.friends[st.session_state.current_chat_id]
+
+    for message in friend.chat_messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+def show_current_chat_head():
+    friend = st.session_state.friends[st.session_state.current_chat_id]
+
+    if user_input := st.chat_input("Start typing..."):
+        with st.chat_message("user"):
+            st.markdown(user_input)
+            
+        with st.chat_message("assistant"):
+            response_generator = friend.interaction_generator(user_input)
+
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in response_generator:
+                full_response += response
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
--- server.py
+++ server.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-from openai import OpenAI
 
-from emobots.emobot import Emobot
-from emobots.personas import get_all_personas
-
-import api_key
+import helper
 
 st.title("Friendface")
 
@@ -15,50 +11,20 @@ if st.button('Reset'):
 
 
 if "friends" not in st.session_state:
-    personas = get_all_personas()
+    helper.initialize_friends()
 
-    friends = {}
-
-    for id, persona in personas.items():
-        client = OpenAI(api_key=api_key.api_key)
-        name = persona["name"]
-        person_desc = persona["description"]
-
-        bot = Emobot(client, name, person_desc)
-        friends[id] = bot
-        print("Created ", name)
-
-    st.session_state.friends = friends
 
 if "current_chat_id" not in st.session_state:
     # get first entry in dictiorary
     st.session_state.current_chat_id = list(st.session_state.friends.keys())[0]
 
-with st.sidebar:
-    for id, friend in st.session_state.friends.items():
-        if st.button(friend.name):
-            st.session_state.current_chat_id = id
-
-st.markdown(st.session_state.current_chat_id)
+helper.show_sidebar()
 
 friend = st.session_state.friends[st.session_state.current_chat_id]
 
+st.markdown(f"You are chatting with *{friend.name}*.")
 
-for message in friend.chat_messages:
-    #st.image(image, caption="vampire", width=100)
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+helper.show_current_chat_tail()
 
-if user_input := st.chat_input("Start typing..."):
-    with st.chat_message("user"):
-        st.markdown(user_input)
-        
-    with st.chat_message("assistant"):
-        response_generator = friend.interaction_generator(user_input)
+helper.show_current_chat_head()
 
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in response_generator:
-            full_response += response
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
